ec18cadfb376dfed76c96cd9d773e21a23da4150
1sanyo <sanyo.adonai@student.moringaschool.com>
2022-08-25 11:37:10 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
 }
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then( function (response) {
+    // console.log(response);
+    return response.json();
+  })
+  .then( function (data) {
+    console.log(data);
+    return renderBooks(data);
+  })
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  };
+
+
+function getTotalPages(books) {
+  let totalPages = 0
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    totalPages += book["numberOfPages"]
   });
+  console.log(`The total pages is ${totalPages}`)
+  console.log(`The fifth book in this series is ${books[4]["name"]}`)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
